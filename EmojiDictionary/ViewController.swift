cd92abf6b713098ac2bc92adaa682673f27c7075
Venkata Sadineni <sadineni.venkat@gmail.com>
2016-12-26 16:11:27 -0800

    Added 2 view controllers

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  
+    @IBOutlet weak var theCoolTableView: UITableView!
+    
+    var people = ["Marc Benioff", "Suresh Bhasyam", "Vanita", "Productvity", "Salesforce", "ATF", "SeTI"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        theCoolTableView.dataSource = self
+        theCoolTableView.delegate = self
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = people[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
+    
 }
 
